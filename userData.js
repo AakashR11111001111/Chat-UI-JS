954da9d982026e3c0102fe520eb25a8cac8c9386
AakashR11111001111 <aakash04721202019@msi-ggsip.org>
2025-02-19 23:40:42 +0530

    bug fixed

--- userData.js
+++ userData.js
@@ -1,89 +1,87 @@
-
-// User Data for dynamic addition of users in user list...........................
 const usersData = [
     {
       avatar:
         "https://hips.hearstapps.com/hmg-prod/images/gettyimages-942065100-copy.jpg?resize=1200:*",
-      name: "John Mayers",
+      name: "Yash Sainani",
       time: "3 min ago",
-      status: "Accountant",
+      status: "Software Engineer",
     },
     {
       avatar:
         "https://cdn.pixabay.com/photo/2021/07/20/14/59/iron-man-6480952_1280.jpg",
-      name: "Tony Stark",
+      name: "Vivek Nag",
       time: "10 min ago",
-      status: "Genius, Billionaire, Playboy, Philanthropist",
+      status: "Web Developer",
     },
     {
       avatar:
         "https://c4.wallpaperflare.com/wallpaper/739/10/526/scarlett-johansson-face-pink-lipstick-actress-wallpaper-preview.jpg",
-      name: "S. Johansson",
+      name: "Sanni Singh",
       time: "3 hours ago",
-      status: "Black Widow",
+      status: "Graphic Designer",
     },
     {
       avatar:
         "https://www.gannett-cdn.com/presto/2023/02/15/USAT/d22ad1a7-00ec-425f-b1c9-a643fcfcda9f-AFP_AFP_336E7FA.jpg",
-      name: "Jeremy Renner",
+      name: "Amit Sharma",
       time: "4 hours ago",
-      status: "Hawkeye",
+      status: "Entrepreneur",
     },
     {
       avatar: "https://flxt.tmsimg.com/assets/308495_v9_bb.jpg",
-      name: "Anthony Mackie",
+      name: "Rohit Verma",
       time: "8 hours ago",
-      status: "Falcon",
+      status: "Digital Marketer",
     },
     {
       avatar:
         "http://t2.gstatic.com/licensed-image?q=tbn:ANd9GcRpW3LbFgFmL3wF7AxVmNPkfKKVTg3hdeDwJAkQ6ffgI1aqNQH-ofOktw9CRPIIPA8IRuPHc9YeW1BGdTI",
-      name: "Chris Evans",
+      name: "Pankaj Mehta",
       time: "22 hours ago",
-      status: "Captain America",
+      status: "Bank Manager",
     },
     {
       avatar:
         "https://hips.hearstapps.com/hmg-prod/images/chris-hemsworth-poses-during-a-photo-call-for-thor-ragnarok-on-october-15-2017-in-sydney-australia-photo-by-mark-metcalfe_getty-images-for-disney-square.jpg?crop=1xw:0.75xh;center,top&resize=1200:*",
-      name: "Chris Hemswoth",
+      name: "Vikram Joshi",
       time: "1 day ago",
-      status: "Thor the god of thunder",
+      status: "Professor",
     },
     {
       avatar: "https://www.emmys.com/sites/default/files/Mark-Ruffalo.jpg",
-      name: "Mark Ruffalo",
+      name: "Ankit Tiwari",
       time: "Dec 10",
-      status: "The Incrdible Hulk",
+      status: "Musician",
     },
     {
       avatar:
         "https://www.themoviedb.org/t/p/w500/mclHxMm8aPlCPKptP67257F5GPo.jpg",
-      name: "Tom Hiddleston",
+      name: "Suraj Yadav",
       time: "Dec 8",
-      status: "Loki",
+      status: "Freelancer",
     },
     {
       avatar: "https://flxt.tmsimg.com/assets/71830_v9_bb.jpg",
-      name: "Don Cheadle",
+      name: "Rajesh Khanna",
       time: "Dec 6",
-      status: "Rhodey",
+      status: "Retired Officer",
     },
     {
       avatar:
         "https://m.media-amazon.com/images/M/MV5BMTk3NDE3Njc5M15BMl5BanBnXkFtZTYwOTY5Nzc1._V1_.jpg",
-      name: "Stan Lee",
+      name: "Devendra Nair",
       time: "Dec 4",
-      status: "The Big Daddy",
+      status: "Startup Founder",
     },
     {
       avatar:
         "https://m.media-amazon.com/images/M/MV5BMTQ1NTQwMTYxNl5BMl5BanBnXkFtZTYwMjA1MzY1._V1_FMjpg_UX1000_.jpg",
-      name: "S.L. Jackson",
+      name: "Prakash Jha",
       time: "Dec 2",
-      status: "Shield",
+      status: "Film Director",
     },
   ];
-  
+
   // Json data ...........................................
   const JsonData = {
     chat1: [
